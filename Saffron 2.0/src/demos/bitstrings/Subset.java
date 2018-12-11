c23979ac26ba4eba74203dba6625eff8cb2a997f
Kerry Soileau <kmsoileau@gmail.com>
2018-12-11 07:42:26 -0600

    Added draft of Subset class.

---
+++ Saffron 2.0/src/demos/bitstrings/Subset.java
@@ -0,0 +1,44 @@
+package demos.bitstrings;
+
+import sets.Set;
+import bits.IBitString;
+import bitstrings.BitString;
+
+public class Subset
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	private final Object[] backing;
+	private IBitString membership;
+
+	public Subset(Object[] backing) throws Exception
+	{
+		this.backing = backing;
+		this.membership = new BitString(backing.length);
+	}
+
+	public Object[] getBacking()
+	{
+		return backing;
+	}
+
+	public IBitString getMembership()
+	{
+		return membership;
+	}
+
+	public void setMembership(IBitString membership)
+	{
+		this.membership = membership;
+	}
+	
+	//To do
+	public Set toSet()
+	{
+		return null;
+	}
+}
