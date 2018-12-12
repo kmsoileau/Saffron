705d59dc16ba0b808210f6aac08b54864858f02e
Kerry Soileau <kmsoileau@gmail.com>
2018-12-12 07:53:13 -0600

    Piton.

--- Saffron 2.0/src/bitstrings/BitStringNoter.java
+++ Saffron 2.0/src/bitstrings/BitStringNoter.java
@@ -5,12 +5,12 @@ import bits.Conjunction;
 import bits.IBitString;
 import bits.IProblem;
 import bits.Problem;
+import exceptions.bitstrings.BitStringNoterException;
 
 /**
- * Constrains the <code>IBooleanVariable</code> at position <code>bit</code> of
- * <code>IBitString Y</code> to equal to the negation of the
- * <code>IBooleanVariable</code> at position <code>bit</code> of
- * <code>IBitString X</code>
+ * For each position <code>bit</code> in <code>IBitString X</code>, the
+ * corresponding <code>IBooleanVariable</code> is constrained to equal the
+ * negation of its counterpart in <code>IBitString Y</code>.
  *
  * @author Kerry Michael Soileau
  *         <p>
@@ -24,6 +24,8 @@ public class BitStringNoter extends Problem implements IProblem
 {
 	public BitStringNoter(IBitString X, IBitString Y) throws Exception
 	{
+		if(X==null || Y==null)
+			throw new BitStringNoterException("Null was passed to constructor for IBitString.");
 		IProblem[] p = new IProblem[X.size()];
 		int count = 0;
 		for (int i = 0; i < p.length; i++)
