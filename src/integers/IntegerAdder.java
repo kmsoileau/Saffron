4a093ed361af494cd6baeedb82d403b008c6c5f6
Kerry Michael Soileau <kerry@kerrysoileau.com>
2020-11-26 18:24:34 -0600

    Added analogous constructors to Integer class.

--- src/integers/IntegerAdder.java
+++ src/integers/IntegerAdder.java
@@ -10,21 +10,26 @@
  */
 package integers;
 
+import java.util.ArrayList;
+
 import bits.BitFixer;
 import bits.Conjunction;
 import bits.Disjunction;
 import bits.IProblem;
 import bits.Problem;
+import integers.exceptions.IntegerAdderException;
+import integers.lists.IIntegerList;
 import naturalnumbers.NaturalNumberAdder;
 import naturalnumbers.NaturalNumberOrderer;
 import naturalnumbers.NaturalNumberStrictOrderer;
 
-/**
- * 
- *
- */
 public class IntegerAdder extends Problem implements IProblem
 {
+	public IntegerAdder(ArrayList<IInteger> addend, IInteger Z)
+	{
+		this(addend.toArray(new IInteger[0]), Z);
+	}
+	
 	public IntegerAdder(IInteger X, IInteger Y, IInteger Z) throws Exception
 	{
 		IProblem[] p = new IProblem[6];
@@ -50,5 +55,53 @@ public class IntegerAdder extends Problem implements IProblem
 		p[5] = new Disjunction(pbfxt, pbfyt, new Conjunction(addxyz, pbfzf));
 
 		this.setClauses(new Conjunction(p).getClauses());
+	}
+	
+	public IntegerAdder(IInteger X, long i, IInteger Z)
+	{
+		try
+		{
+			IInteger C1 = new Integer();
+			IProblem p3 = new Conjunction(new IntegerFixer(C1, i), new IntegerAdder(X, C1, Z));
+			this.setClauses(p3.getClauses());
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public IntegerAdder(IInteger[] addend, IInteger Z)
+	{
+		try
+		{
+			int sz = addend.length;
+			if (sz == 0)
+				throw new IntegerAdderException("Empty IInteger array passed to constructor.");
+			else
+			{
+				IProblem[] p = new IProblem[sz];
+
+				IInteger[] Zz = new IInteger[sz - 1];
+				Zz[0] = new Integer();
+				p[0] = new IntegerEqualizer(addend[0], Zz[0]);
+				for (int i = 1; i < sz - 1; i++)
+				{
+					Zz[i] = new Integer();
+					p[i] = new IntegerAdder(Zz[i - 1], addend[i], Zz[i]);
+				}
+				p[sz - 1] = new IntegerAdder(Zz[sz - 2], addend[sz - 1], Z);
+
+				IProblem prob = new Conjunction(p);
+				this.setClauses(prob.getClauses());
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public IntegerAdder(IIntegerList addend, IInteger Z)
+	{
+		this(addend.getIntegerArray(), Z);
 	}
 }
---
+++ src/integers/exceptions/IntegerAdderException.java
@@ -0,0 +1,12 @@
+package integers.exceptions;
+
+public class IntegerAdderException extends Exception
+{
+	private static final long serialVersionUID = -2672539816311161524L;
+
+	public IntegerAdderException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
